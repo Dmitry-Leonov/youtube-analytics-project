75f1295074eac0026e3c0416a7bced1863f3ef18
Dmitry Leonov <leonov.do72@gmail.com>
2023-07-25 23:53:27 +0300

    Домашка 4 (Ютуб)

--- src/channel.py
+++ src/channel.py
@@ -8,7 +8,7 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """ Класс для ютуб-канала """
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
---
+++ src/video.py
@@ -0,0 +1,34 @@
+import os
+
+from googleapiclient.discovery import build
+
+api_key: str = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+class Video:
+    def __init__(self, video_id):
+        """Инициализация по id видео Ютуб"""
+        self.video_id = video_id
+        self.video_title: str = self.video_response['items'][0]['snippet']['title']
+        self.video_url: str = 'https://youtu.be/' + self.video_id
+        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+
+    @property
+    def video_response(self):
+        return youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                     id=self.video_id).execute()
+
+    def __str__(self):
+        return f'{self.video_title}'
+
+
+class PLVideo(Video):
+    """Дочерний класс от Video с добавлением id плейлиста"""
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
+        
+    def __str__(self):
+        return super().__str__()
